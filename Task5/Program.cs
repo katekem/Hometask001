92874cd4b9c06ca9f47193d5be52bdfd33b54f3a
Kate <vvellspring@gmail.com>
2022-08-12 22:19:35 +0300

    Решение Задачи5

---
+++ Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 5: Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int result = - N;
+while(result <= N)
+{
+    Console.Write(result);
+    result ++;
+};
+
+
