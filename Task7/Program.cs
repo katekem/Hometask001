dd5a2b90ad51a3c16ddf29d6022cf342e7bb5a73
Kate <vvellspring@gmail.com>
2022-08-12 22:27:56 +0300

    Решение Задачи7

---
+++ Task7/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 7: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает последнюю цифру этого числа.
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if(N < 99 || N > 999)
+Console.WriteLine("Это число не трехзначное");
+else
+{
+    int result = N%10;
+    Console.WriteLine("Последняя цифра этого числа: " + result);
+}
